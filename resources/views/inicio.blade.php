c8f79601bde3c6965da4258ca447c8e0e35603fb
William May <williammayy@gmail.com>
2021-07-28 15:47:27 -0300

    view inicial

---
+++ resources/views/inicio.blade.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html lang="pt-br">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>URLs</title>
+        <link rel="stylesheet" href="{{asset('css/app.css')}}">
+    </head>
+    <body>
+        
+
+
+
+        <script src="{{asset('js/app.js')}}"></script>
+    </body>
+</html>
