e4acd49b62e70964c2624ac97ee59fc388b83634
William May <williammayy@gmail.com>
2021-07-28 16:13:48 -0300

    Adicionado input da url e botão de envio

--- resources/views/inicio.blade.php
+++ resources/views/inicio.blade.php
@@ -5,12 +5,28 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>URLs</title>
         <link rel="stylesheet" href="{{asset('css/app.css')}}">
+        <style>
+            body{
+                margin-top: 10px;
+            }
+        </style>
     </head>
     <body>
-        
+        <div class="container">
+
+            <h2 class="text-center">Cadastre sua URL</h2>
+            <form action="">
 
 
+                <div class="input-group input-group-lg">
+                    <input type="text" class="form-control border-primary" placeholder="https://exemplo.com.br">
+                    <div class="input-group-append">
+                      <button class="btn btn-primary" type="submit">Cadastrar</button>
+                    </div>
+                </div>
+            </form>
 
+        </div>
         <script src="{{asset('js/app.js')}}"></script>
     </body>
 </html>
